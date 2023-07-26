a3153db295c5a4fdddf8629a35b4cfb34bd5f225
Michael La Plante <michael@michaellaplante.com>
2023-07-26 15:31:45 -0400

    remove analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,39 @@
-import Script from 'next/script';
-import Head from 'next/head';
-import styles from '@/styles/Home.module.scss';
-import HomePage from '../components/Home/HomePage';
-import Header from '../components/Header/Header';
+import Script from "next/script";
+import Head from "next/head";
+import styles from "@/styles/Home.module.scss";
+import HomePage from "../components/Home/HomePage";
+import Header from "../components/Header/Header";
 
 export default function Home() {
-
   return (
     <>
       <Head>
         <title>Invoice Dragon</title>
-        <title>Invoice Dragon - Create PDF Invoices and Receipts for Free</title>
-        <meta property="og:title" content="Invoice Dragon - Create PDF Invoices and Receipts for Free" />
+        <title>
+          Invoice Dragon - Create PDF Invoices and Receipts for Free
+        </title>
+        <meta
+          property="og:title"
+          content="Invoice Dragon - Create PDF Invoices and Receipts for Free"
+        />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="http://invoicedragon.com/" />
-        <meta property="og:image" content="/assets/icon.png" /> 
-        <meta property="og:description" content="Create professional Invoices and Receipts effortlessly with Invoice Dragon and download as PDF for easy sharing and record-keeping. And it's all for free!" />
-        <meta property="og:locale" content="en_US" />
-        <meta property="og:site_name" content="Invoice Dragon" />
-        <meta name="author" content="Lani" />
-        <meta name="description" content="Create professional Invoices and Receipts effortlessly with Invoice Dragon and download as PDF for easy sharing and record-keeping. And it's all for free!" />
+        <meta property="og:image" content="/assets/icon.png" />
+        <meta
+          property="og:description"
+          content="Create professional Invoices and Receipts effortlessly with Invoice Dragon and download as PDF for easy sharing and record-keeping. And it's all for free!"
+        />
+        <meta
+          name="description"
+          content="Create professional Invoices and Receipts effortlessly with Invoice Dragon and download as PDF for easy sharing and record-keeping. And it's all for free!"
+        />
         <meta name="application-name" content="Invoice Dragon" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-LHPWMTY60Z"></Script>
-      <Script id="google-analytics">
-        {`window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-LHPWMTY60Z');`}
-      </Script>
       <main className={styles.main}>
         <Header />
         <HomePage />
       </main>
     </>
-  )
+  );
 }
--- src/pages/templates.js
+++ src/pages/templates.js
@@ -1,31 +1,31 @@
-import Head from 'next/head';
-import Script from 'next/script';
+import Head from "next/head";
+import Script from "next/script";
 
-import { useState, useEffect } from 'react';
-import { useMediaQuery } from 'react-responsive';
+import { useState, useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
 
-import { PDFDownloadLink } from '@react-pdf/renderer';
-import { PDF } from '../components/Preview/Preview';
+import { PDFDownloadLink } from "@react-pdf/renderer";
+import { PDF } from "../components/Preview/Preview";
 import InvoiceTemplate from "../components/InvoiceTemplate/InvoiceTemplate";
-import Dropdown from '../components/Dropdown/Dropdown';
-import styles from '@/styles/Home.module.scss';
+import Dropdown from "../components/Dropdown/Dropdown";
+import styles from "@/styles/Home.module.scss";
 import Form from "../components/Form/Form";
-import Header from '@/components/Header/Header';
-import logoP from '../assets/images/placeholder-image.png';
+import Header from "@/components/Header/Header";
+import logoP from "../assets/images/placeholder-image.png";
 import Previewed from "../components/Preview/Preview";
-
-
 
 const Templates = () => {
   // const [service, setService] = useState('invoice');
 
   const [showPreview, setShowPreview] = useState(false);
-  const [formData, setFormData] = useState({formName: 'Invoice'});
-  const [rows, setRows] = useState(Array(1).fill({id: 0, quantity: 1, amount: '0.00'}));
+  const [formData, setFormData] = useState({ formName: "Invoice" });
+  const [rows, setRows] = useState(
+    Array(1).fill({ id: 0, quantity: 1, amount: "0.00" })
+  );
   const [logo, setLogo] = useState(logoP);
   const [logoUpdated, setLogoUpdated] = useState(false);
-  const [currencySymbol, setCurrencySymbol] = useState('$');
-  const [currencyCode, setCurrencyCode] = useState('USD');
+  const [currencySymbol, setCurrencySymbol] = useState("$");
+  const [currencyCode, setCurrencyCode] = useState("USD");
   const [template, setTemplate] = useState(null);
   const [templateSelected, setTemplateSelected] = useState(false);
   const [total, setTotal] = useState(0);
@@ -38,12 +38,11 @@ const Templates = () => {
 
     setTimeout(() => {
       window.scroll({
-        top: isMobile? 1800 : 500,
+        top: isMobile ? 1800 : 500,
         behavior: "smooth",
       });
-    }, 400)
-
-  }
+    }, 400);
+  };
 
   const handleLogoUpdate = (e) => {
     const reader = new FileReader();
@@ -52,46 +51,53 @@ const Templates = () => {
         setLogo(reader.result);
         setLogoUpdated(true);
       }
-    }
+    };
     reader.readAsDataURL(e.target.files[0]);
-  }
+  };
 
   const handleFormChange = (name, value) => {
     setFormData({
-      ...formData, [name]: value
-    })
+      ...formData,
+      [name]: value,
+    });
   };
   const handleToggle = () => {
     setShowPreview(!showPreview);
-  }
+  };
 
   // Table Functions
   const handleTableUpdate = (e, id, amount) => {
     setRows((prevRows) => {
       const updateTable = [...prevRows];
       const currentRowIndex = updateTable.findIndex((row) => row.id === id);
-      updateTable[currentRowIndex] = { ...updateTable[currentRowIndex], [e.target.name]: e.target.value }
-      if ( amount !== undefined) {
+      updateTable[currentRowIndex] = {
+        ...updateTable[currentRowIndex],
+        [e.target.name]: e.target.value,
+      };
+      if (amount !== undefined) {
         updateTable[currentRowIndex].amount = amount;
       }
-      if (e.target.name === 'rate' || e.target.name === 'quantity' ) {
+      if (e.target.name === "rate" || e.target.name === "quantity") {
         updateTable[currentRowIndex][e.target.name] = Number(e.target.value);
       }
       return updateTable;
     });
-  }
+  };
   const handleRowAdd = () => {
     const lastId = rows.length ? rows[rows.length - 1].id : 0;
-    setRows((prevRows) => [...prevRows, {id: lastId + 1, quantity: 1, amount: '0.00'}]);
-  }
+    setRows((prevRows) => [
+      ...prevRows,
+      { id: lastId + 1, quantity: 1, amount: "0.00" },
+    ]);
+  };
 
   const handleRowRemove = (id) => {
-    setRows((prevRows) => prevRows.filter(item => item.id !== id));
-  }
+    setRows((prevRows) => prevRows.filter((item) => item.id !== id));
+  };
   const handleCurrencyModify = (curr) => {
-    setCurrencyCode(curr.code)
+    setCurrencyCode(curr.code);
     setCurrencySymbol(curr.symbol);
-  }
+  };
 
   function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -99,16 +105,15 @@ const Templates = () => {
 
   const calculateTotal = () => {
     let sum = 0;
-    rows.forEach(row => {
+    rows.forEach((row) => {
       sum += parseFloat(row.amount);
-    })
+    });
     setTotal(numberWithCommas(sum.toFixed(2)));
-  }
+  };
 
   useEffect(() => {
     calculateTotal();
   }, [rows]);
-
 
   const pdf = (
     <PDF
@@ -139,7 +144,7 @@ const Templates = () => {
     />
   );
 
-  return ( 
+  return (
     <>
       <Head>
         <title>Invoice Dragon</title>
@@ -147,14 +152,6 @@ const Templates = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-LHPWMTY60Z"></Script>
-      <Script id="google-analytics">
-        {`window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-LHPWMTY60Z');`}
-      </Script>
       <main>
         <div className={styles.template__wrapper}>
           <div className={styles.header}>
@@ -165,64 +162,81 @@ const Templates = () => {
               template={template}
               changeTemplate={handleTemplateChange}
             />
-            {templateSelected && <div className={styles.template__section}>
-              <div className={styles.main__section}>
-                {!showPreview && <Form
-                  prefill={formData}
-                  rows={rows}
-                  logo={logo}
-                  updateLogo={handleLogoUpdate}
-                  logoUpdated={logoUpdated}
-                  currencySymbol={currencySymbol}
-                  onFormMod={handleFormChange}
-                  onPreviewToggle={handleToggle}
-                  onRowAdd={handleRowAdd}
-                  onRowRemove={handleRowRemove}
-                  onTableUpdate={handleTableUpdate}
-                  />}
-                
-                {showPreview && <Previewed 
-                  {...formData}
-                  rows={rows}
-                  logo={logo}
-                  logoUpdated={logoUpdated}
-                  template={template}
-                  currencySymbol={currencySymbol}
-                  onPreviewToggle={handleToggle}
-                />}
-              </div>
-              <div className={styles.action__section}>
-                <div className={styles.actions}>
-                  ACTIONS
-                  <br />
-                  <br />
-                  <button className={styles.action__btn} onClick={handleToggle}>{showPreview ? 'Back to Edit' : 'Preview Invoice'}</button>
-                  <br />
-                  <br />
-                  <div>
-                    <PDFDownloadLink 
-                      document={pdf}
-                      fileName={`${formData.clientName}_${formData.formName}.pdf`}>
-                      {({ blob, url, loading, error }) =>
-                        <button className={styles.action__btn} disabled={!showPreview}>Download PDF</button> 
-                      }
-                    </PDFDownloadLink>
+            {templateSelected && (
+              <div className={styles.template__section}>
+                <div className={styles.main__section}>
+                  {!showPreview && (
+                    <Form
+                      prefill={formData}
+                      rows={rows}
+                      logo={logo}
+                      updateLogo={handleLogoUpdate}
+                      logoUpdated={logoUpdated}
+                      currencySymbol={currencySymbol}
+                      onFormMod={handleFormChange}
+                      onPreviewToggle={handleToggle}
+                      onRowAdd={handleRowAdd}
+                      onRowRemove={handleRowRemove}
+                      onTableUpdate={handleTableUpdate}
+                    />
+                  )}
+
+                  {showPreview && (
+                    <Previewed
+                      {...formData}
+                      rows={rows}
+                      logo={logo}
+                      logoUpdated={logoUpdated}
+                      template={template}
+                      currencySymbol={currencySymbol}
+                      onPreviewToggle={handleToggle}
+                    />
+                  )}
+                </div>
+                <div className={styles.action__section}>
+                  <div className={styles.actions}>
+                    ACTIONS
+                    <br />
+                    <br />
+                    <button
+                      className={styles.action__btn}
+                      onClick={handleToggle}
+                    >
+                      {showPreview ? "Back to Edit" : "Preview Invoice"}
+                    </button>
+                    <br />
+                    <br />
+                    <div>
+                      <PDFDownloadLink
+                        document={pdf}
+                        fileName={`${formData.clientName}_${formData.formName}.pdf`}
+                      >
+                        {({ blob, url, loading, error }) => (
+                          <button
+                            className={styles.action__btn}
+                            disabled={!showPreview}
+                          >
+                            Download PDF
+                          </button>
+                        )}
+                      </PDFDownloadLink>
+                    </div>
+                    <br />
+                    <br />
+                    <Dropdown
+                      currencyCode={currencyCode}
+                      currencySymbol={currencySymbol}
+                      onCurrencyModify={handleCurrencyModify}
+                    />
                   </div>
-                  <br />
-                  <br />
-                  <Dropdown
-                    currencyCode={currencyCode}
-                    currencySymbol={currencySymbol}
-                    onCurrencyModify={handleCurrencyModify}
-                  />
                 </div>
               </div>
-            </div>}
+            )}
           </div>
         </div>
       </main>
     </>
-   );
-}
- 
+  );
+};
+
 export default Templates;
